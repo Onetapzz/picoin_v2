7d090cc4021b6a82691ff4506dd6065a04241f69
Lensaa00 <jaxis1818@gmail.com>
2024-06-09 23:46:16 +1100

    fixed screen ratio (maybe) + some little fixes

--- app/handlers.py
+++ app/handlers.py
@@ -9,11 +9,10 @@ import config
 router = Router()
 
 
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer_photo(photo=config.START_BANNER,
                                caption=f"*Hello, @{message.from_user.username}! Welcome to PiCoin! 🥇*",
                                reply_markup=kb.main,
                                parse_mode=ParseMode.MARKDOWN)
-    # await message.answer("*Hello! Welcome to PiCoin! 🥇*",
-    #                      reply_markup=kb.main, parse_mode=ParseMode.MARKDOWN)
